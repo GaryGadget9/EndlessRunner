9013fed423873e6251fc1cc341e12367a99dc6d8
vegetables1234 <vsudesh123@gmail.com>
2020-07-19 15:44:03 +1000

    made a runnning game i guess

---
+++ Assets/GameManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public Transform platformGenerator;
+    private Vector3 platformStartPoint;
+
+    public PlayerControl thePlayer;
+    private Vector3 playerStartPoint;
+
+    private CubeScript[] platformList;
+
+    private TimerManager timeManager;
+
+    public DeathMenu theDeathScreen;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        platformStartPoint = platformGenerator.position;
+        playerStartPoint = new Vector3(this.transform.position.x, this.transform.position.y + 10);
+        //thePlayer.transform.position
+        timeManager = FindObjectOfType<TimerManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void RestartGame()   //calls the Coroutine RestartGameCo
+    {
+        timeManager.timerIncreasing = false;
+        thePlayer.gameObject.SetActive(false);
+        //StartCoroutine("RestartGameCo");
+
+        theDeathScreen.gameObject.SetActive(true);
+        print("hello2");
+
+    }
+
+    public void Reset()
+    {
+        SceneManager.LoadScene("Nova");
+        
+    }
+
+
+        
+} 
+
---
+++ Assets/script/SpriteFlipper.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteFlipper : MonoBehaviour
+{
+    //this isn't used anymore
+
+    private SpriteRenderer mrBandit;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        mrBandit = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mrBandit.flipX = true;
+    }
+}
